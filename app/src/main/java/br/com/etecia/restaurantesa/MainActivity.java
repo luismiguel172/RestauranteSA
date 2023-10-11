c0098fd622cbbb223733a98388c7d86d18f80349
luismiguel172 <luis.goncalves68@etec.sp.gov.br>
2023-10-10 22:04:52 -0300

    second commit

--- app/src/main/java/br/com/etecia/restaurantesa/MainActivity.java
+++ app/src/main/java/br/com/etecia/restaurantesa/MainActivity.java
@@ -3,6 +3,10 @@ package br.com.etecia.restaurantesa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.avaliacao, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+
+        adapter.setDropDownViewResource(com.google.android.material.R.layout.support_simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
     }
 }
